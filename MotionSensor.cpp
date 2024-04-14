631c32faf7b425ac58fbbdea65dd58a8dbc986dc
yokinoh <545225602@qq.com>
2024-04-14 13:19:54 +0100

    JIANHUADONG Merge update and upload drivers

--- MotionSensor.cpp
+++ MotionSensor.cpp
@@ -1,12 +1,21 @@
 #include "MotionSensor.h"
+#include <iostream>
+#include <pigpio.h>
 
+#define MPU_INT_PIN 17
 
 #define wrap_180(x) (x < -180 ? x+360 : (x > 180 ? x - 360: x))
 #define delay_ms(a)    usleep(a*1000)
 
 MotionSensor::MotionSensor()
 {
-
+	if (gpioInitialise() < 0) {
+		std::cerr << "pigpio initialization failed." << std::endl;
+	}
+}
+// MotionSensor destructor
+MotionSensor::~MotionSensor() {
+	gpioTerminate();
 }
 
 int MotionSensor::ms_open() 
@@ -19,137 +28,150 @@ int MotionSensor::ms_open()
 	}
 
 	// initialize device
-	printf("初始化MPU...请等待\n");
+	printf("Initializing the MPU... Please wait.\n");
 	if (mpu_init(NULL) != 0) {
-		printf("MPU初始化失败.\n");
+		printf("MPU initialization failure.\n");
 		return -1;
 	}
-	printf("MPU初始化成功.");
+	printf("MPU initialization is successful..");
 
-	printf("设置MPU传感器...请等待\n");
+	printf("Setting up the MPU sensor... Please wait.\n");
     dump_init();
 	if (mpu_set_sensors(INV_XYZ_GYRO|INV_XYZ_ACCEL)!=0) {
-		printf("MPU传感器设置失败.\n");
+		printf("MPU sensor setting failure.\n");
 		return -1;
 	}
-	printf("MPU传感器设置成功.\n");
+	printf("MPU Sensor Setting Successful.\n");
 
-	printf("设置陀螺仪灵敏度...请等待\n");
+	printf("Setting gyro sensitivity... Please wait.\n");
 	if (mpu_set_gyro_fsr(2000)!=0) {
-		printf("陀螺仪精度设置失败.\n");
+		printf("Failed to set gyro accuracy.\n");
 		return -1;
 	}
-	printf("陀螺仪精度设置成功.\n");
+	printf("Gyroscope accuracy set successfully.\n");
 
-	printf("设置加速度计灵敏度...请等待\n");
+	printf("Setting accelerometer sensitivity... Please wait.\n");
 	if (mpu_set_accel_fsr(2)!=0) {
-		printf("加速度计灵敏度设置失败.\n");
+		printf("Accelerometer sensitivity setting failed.\n");
 		return -1;
 	}
-	printf("加速度计灵敏度设置成功.\n");
+	printf("Accelerometer sensitivity set successfully.\n");
 
 	// verify connection
-	printf("执行MPU开机命令...请等待\n");
+	printf("Execute the MPU boot command... Please wait.\n");
 	mpu_get_power_state(&devStatus);
-	printf(devStatus ? "MPU6050 连接成功.\n" : "MPU6050 连接失败. %u\n",devStatus);
+	printf(devStatus ? "MPU6050 Connection Successful.\n" : "MPU6050 Connection Failure. %u\n",devStatus);
 
 	//fifo config
-	printf("设置MPU FIFO...请等待\n");
+	printf("Setting the MPU FIFO... Please wait\n");
 	if (mpu_configure_fifo(INV_XYZ_GYRO|INV_XYZ_ACCEL)!=0) {
 		printf("MPU fifo 设置失败.\n");
 		return -1;
 	}
-	printf("MPU fifo 设置成功.\n");
+	printf("MPU fifo set successfully.\n");
 
 
 	// load and configure the DMP
-	printf("读取DMP固件...请等待\n");
+	printf("Reading DMP firmware... Please wait.\n");
 	if (dmp_load_motion_driver_firmware()!=0) {
-		printf("DMP固件打开失败.\n");
+		printf("DMP firmware open failure.\n");
 		return -1;
 	}
-	printf("DMP固件打开成功.\n");
+	printf("DMP firmware opened successfully.\n");
 
 
-	printf("正在激活DMP...请等待\n");
+	printf("Activating DMP... Please wait.\n");
 	if (mpu_set_dmp_state(1)!=0) {
 		printf("DMP激活失败.\n");
 		return -1;
 	}
-	printf("DMP激活成功.\n");
+	printf("DMP activation successful.\n");
 
 	//dmp_set_orientation()
 	//if (dmp_enable_feature(DMP_FEATURE_LP_QUAT|DMP_FEATURE_SEND_RAW_GYRO)!=0) {
-	printf("设置DMP...请等待\n");
+	printf("Setting the DMP... Please wait.\n");
 	if (dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT|DMP_FEATURE_SEND_RAW_ACCEL|DMP_FEATURE_SEND_CAL_GYRO|DMP_FEATURE_GYRO_CAL)!=0) {
-		printf("DMP特征提取失败.\n");
+		printf("DMP feature extraction failed.\n");
 		return -1;
 	}
-	printf("DMP特征提取成功.\n");
+	printf("DMP feature extraction successful.\n");
 
-	printf("设置DMP FIFO采样率...请等待\n");
+	printf("Setting the DMP FIFO sample rate... Please wait\n");
 	if (dmp_set_fifo_rate(rate)!=0) {
-		printf("DMP FIFO采样率设置失败.\n");
+		printf("DMP FIFO Sample Rate Setting Failure.\n");
 		return -1;
 	}
-	printf("DMP FIFO采样率设置成功.\n");
+	printf("DMP FIFO Sample Rate Setting Successful.\n");
 
 
-	printf("重置FIFO消息队列...请等待\n");
+	printf("Reset FIFO message queue... Please wait\n");
 	if (mpu_reset_fifo()!=0) {
-		printf("FIFO消息队列重置失败.\n");
+		printf("FIFO message queue reset failure.\n");
 		return -1;
 	}
-	printf("FIFO消息队列重置成功.\n");
+	printf("FIFO message queue reset successful.\n");
 
-	printf("检测中... ");
+	printf("under test... ");
 	do {
 		delay_ms(1000/rate);  //dmp will habve 4 (5-1) packets based on the fifo_rate
 		r=dmp_read_fifo(g,a,_q,&sensors,&fifoCount);
 	} while (r!=0 || fifoCount<5); //packtets!!!
-	printf("完成.\n");
+	printf("fulfillment.\n");
 
 	initialized = 1;
+	// Set GPIO mode and interrupts
+	gpioSetMode(MPU_INT_PIN, PI_INPUT);// Set GPIOs as inputs
+	gpioSetPullUpDown(MPU_INT_PIN, PI_PUD_DOWN);// Setting up internal dropdowns
+	gpioSetISRFuncEx(MPU_INT_PIN, RISING_EDGE, 0, interruptHandler, this);// Register the interrupt handler
 	return 0;
+}
+
+void interruptHandler(int gpio, int level, uint32_t tick, void* user) {
+	MotionSensor* sensor = reinterpret_cast<MotionSensor*>(user);
+	sensor->ms_update();
 }
 
 int MotionSensor::ms_update() 
 {
 	if (!dmpReady) {
-		printf("错误: DMP未准备完成.\n");
+		printf("Error: DMP not ready.\n");
 		return -1;
 	}
 
-	while (dmp_read_fifo(g,a,_q,&sensors,&fifoCount)!=0); //gyro and accel can be null because of being disabled in the efeatures
-	q = _q;
-	GetGravity(&gravity, &q);
-	GetYawPitchRoll(ypr, &q, &gravity);
+	if (dmp_read_fifo(g, a, _q, &sensors, &fifoCount) == 0) {
+		q = _q;
+		GetGravity(&gravity, &q);
+		GetYawPitchRoll(ypr, &q, &gravity);
 
-	mpu_get_temperature(&t);
-	temp=(float)t/65536L;
+		mpu_get_temperature(&t);
+		temp = (float)t / 65536L;
 
-	mpu_get_compass_reg(c);
+		mpu_get_compass_reg(c);
 
-	//scaling for degrees output
-	for (int i=0;i<DIM;i++){
-		ypr[i]*=180/M_PI;
+		//scaling for degrees output
+		for (int i = 0; i < DIM; i++) {
+			ypr[i] *= 180 / M_PI;
+		}
+
+		//unwrap yaw when it reaches 180
+		ypr[0] = wrap_180(ypr[0]);
+
+		//change sign of Pitch, MPU is attached upside down
+		ypr[1] *= -1.0;
+
+		//0=gyroX, 1=gyroY, 2=gyroZ
+		//swapped to match Yaw,Pitch,Roll
+		//Scaled from deg/s to get tr/s
+		for (int i = 0; i < DIM; i++) {
+			gyro[i] = (float)(g[DIM - i - 1]) / 131.0 / 360.0;
+			accel[i] = (float)(a[DIM - i - 1]);
+			compass[i] = (float)(c[DIM - i - 1]);
+		}
+	}else {
+		// Log or handle error
+		std::cerr << "Failed to read FIFO data." << std::endl;
 	}
-
-	//unwrap yaw when it reaches 180
-	ypr[0] = wrap_180(ypr[0]);
-
-	//change sign of Pitch, MPU is attached upside down
-	ypr[1]*=-1.0;
-
-	//0=gyroX, 1=gyroY, 2=gyroZ
-	//swapped to match Yaw,Pitch,Roll
-	//Scaled from deg/s to get tr/s
-	for (int i=0;i<DIM;i++){
-		gyro[i]   = (float)(g[DIM-i-1])/131.0/360.0;
-		accel[i]   = (float)(a[DIM-i-1]);
-		compass[i] = (float)(c[DIM-i-1]);
-	}
-
+	
 	return 0;
 }
 
